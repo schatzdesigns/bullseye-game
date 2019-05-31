15d193209982afdbf768286d50c1b5be4f0196ad
Zakayo Thuku <zakayothuku@gmail.com>
2019-05-31 10:17:25 +0300

    Initial project Setup Added button Added Alert when Button is clicked

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "hello world", message: "This is my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
